790ede384fad1ad5e72512ce57bb92f88aaeff4c
615141048312 <hanada.yui@mbr.nifty.com>
2020-10-01 22:10:15 +0900

    finish 1

---
+++ src/KickingHuman.java
@@ -0,0 +1,8 @@
+public class KickingHuman {
+	public static void main(String[] args) {
+		Human human = new Human();
+		Human.HumanLeg humanLeg = human.new HumanLeg();
+		
+		humanLeg.kick();
+	}
+}
