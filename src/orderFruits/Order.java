6c016011454b9ef5fb7e8a1ecdfa3fd029814631
615141048312 <hanada.yui@mbr.nifty.com>
2020-10-16 17:40:54 +0900

    7-2

--- src/orderFruits/Order.java
+++ src/orderFruits/Order.java
@@ -1,7 +1,7 @@
 package orderFruits;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+//import java.util.Comparator;
 import java.util.List;
 
 public class Order {
@@ -14,27 +14,11 @@ public class Order {
 		fruits.add(new Fruit("メロン", 8));
 		fruits.add(new Fruit("ぶどう", 20));
 		
-		//ラムダ式を使わない場合
-		List<Fruit> filtered = new ArrayList<>();
-		for (Fruit fruit : fruits) {
-			if (fruit.quantity <= 10) {
-				filtered.add(fruit);
-			}
-		}
-		
-		List<Fruit> ordered = new ArrayList<>();
-		for (Fruit fruit : filtered) {
-			ordered.add(fruit.order(20));
-		}
-		ordered.sort(new Comparator<Fruit>() {
-			@Override
-			public int compare(Fruit f1, Fruit f2) {
-				return f1.quantity - f2.quantity;
-			}
-		});
-		
-		for (Fruit fruit : ordered) {
-			System.out.println(fruit);
-		}
+		//ラムダ式を使う場合
+		fruits.stream() // Streamを生成
+	    .filter(f -> f.quantity <= 10) // 中間操作1: 個数が10個以下のものを抽出
+	    .map(f -> f.order(20)) // 中間操作2: 20個ずつ発注
+	    .sorted((f1, f2) -> f1.quantity - f2.quantity) // 中間操作3: 個数の少ない順に並び替え
+	    .forEach(System.out::println); // 終端操作: 標準出力
 	}
 }
